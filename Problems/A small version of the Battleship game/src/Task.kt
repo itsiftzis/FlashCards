c466105fa50d369e8bb589a3caf6add1405a92e1
Ioannis Tsiftzis <ioannis.tsiftzis@gmail.com>
2020-07-06 17:18:26 +0300

    next exercise

--- Problems/A small version of the Battleship game/src/Task.kt
+++ Problems/A small version of the Battleship game/src/Task.kt
@@ -2,5 +2,32 @@ import java.util.Scanner
 
 fun main(args: Array<String>) {
     val input = Scanner(System.`in`)
-    // put your code here
+    val firstShip = Pair(input.nextInt(), input.nextInt())
+    val secondShip = Pair(input.nextInt(), input.nextInt())
+    val thirdShip = Pair(input.nextInt(), input.nextInt())
+
+    var rows = mutableListOf<Int>()
+    var columns = mutableListOf<Int>()
+
+    for (x in 1..5) {
+        if (allowedRow(x, firstShip) && allowedRow(x, secondShip) && allowedRow(x, thirdShip)) {
+            rows.add(x)
+        }
+    }
+    for (y in 1..5) {
+        if (allowedColumn(y, firstShip) && allowedColumn(y, secondShip) && allowedColumn(y, thirdShip)) {
+            columns.add(y)
+        }
+    }
+
+    println(rows.joinToString(" "))
+    println(columns.joinToString(" "))
+
 }
+
+private fun allowedColumn(y: Int, first: Pair<Int, Int>) =
+        y != first.second
+
+
+private fun allowedRow(x: Int, firstShip: Pair<Int, Int>) =
+        x != firstShip.first
